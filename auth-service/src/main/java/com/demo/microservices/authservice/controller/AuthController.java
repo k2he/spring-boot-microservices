9741c250b2fc80826180d3515bc59e6f312fbe82
k2he <he_kai517@hotmail.com>
2019-04-06 23:10:28 -0400

    How code looks in Gibhub after formatted with  Google Format.

--- auth-service/src/main/java/com/demo/microservices/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/demo/microservices/authservice/controller/AuthController.java
@@ -1,15 +1,12 @@
 package com.demo.microservices.authservice.controller;
 
 import java.util.Base64;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.demo.microservices.authservice.security.RSAKeys;
 import com.demo.microservices.servicelibs.security.JwtPublicKey;
-
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
@@ -17,14 +14,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
-	private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+  private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
-	@NonNull
-	private RSAKeys jwtKeys;
+  @NonNull
+  private RSAKeys jwtKeys;
 
-	@GetMapping("/public-key")
-	public JwtPublicKey getPublicKey() {
-		String publicKeyStr = Base64.getEncoder().encodeToString(jwtKeys.getPublicKey().getEncoded());
-		return new JwtPublicKey(publicKeyStr);
-	}
+  @GetMapping("/public-key")
+  public JwtPublicKey getPublicKey() {
+    String publicKeyStr = Base64.getEncoder().encodeToString(jwtKeys.getPublicKey().getEncoded());
+    return new JwtPublicKey(publicKeyStr);
+  }
 }
